605cc25ecea1d121005bb967add4f0ddc6a43dd9
Danila Nikolaev <danila_nikolaev@epam.com>
2024-07-26 17:20:16 +0400

    initial setup

---
+++ lambda_function.py
@@ -0,0 +1,9 @@
+
+
+
+
+def lambda_handler(event, context):
+
+    return 0;
+
+
---
+++ telegram_monitor.py
@@ -0,0 +1,13 @@
+
+
+class TelegramMonitor:
+    def __init__(self) -> None:
+        # TODO: init keywords and channel names into class fields
+        pass
+
+
+    def run() -> None:
+        # get all new mesages from channels
+
+        # forward the ones that contain keywords to user
+        return None;
