2e2a81a638ec38f27051b1cbad5b20c918d175e2
dartungar <dartungar@gmail.com>
2024-11-03 13:05:31 +0400

    small refactoring

--- lambda_function.py
+++ lambda_function.py
@@ -10,51 +10,54 @@ def lambda_handler(event, context):
     api_hash = os.getenv('API_HASH')
     string_session = os.getenv('string_session')
 
+    if not all([api_id, api_hash, string_session]):
+        raise ValueError("API_ID, API_HASH, and string_session must be set in environment variables.")
+
     # Define the list of channels to monitor and keywords
-    channels = event['channels'] if 'channels' in event else ['baraholka_tbi', 'tbilisio', 'tbilisiw', 'tbilisy_batumy', 'baraholka_tbilisi_home', 'avito_baraholka_tbilisi', 'tbilisi_baraxolka', 'baraholka_tbilisi_ge']
-    keywords = event['keywords'] if 'keywords' in event else ['ноутбук', 'лэптоп', "laptop"] 
-    stopwords = event['stopwords'] if 'stopwords' in event else ['куплю']
+    channels = event.get('channels', [
+        'baraholka_tbi', 'tbilisio', 'tbilisiw', 'tbilisy_batumy',
+        'baraholka_tbilisi_home', 'avito_baraholka_tbilisi',
+        'tbilisi_baraxolka', 'baraholka_tbilisi_ge'
+    ])
+    keywords = event.get('keywords', ['ноутбук', 'лэптоп', "laptop"])
+    stopwords = event.get('stopwords', ['куплю'])
 
     # Create the client
     client = TelegramClient(StringSession(string_session), api_id, api_hash)
-
-    msg_count = 0;
 
     async def main():
         # Connect to the client
         await client.start()
 
-        dialogs = await client.get_dialogs()
         channel_to_forward = await client.get_entity('https://t.me/+jNtkTIzX8NhlNWEy')  # Replace with your channel username/ID
 
-        # TODO: pass keywords & channels to lambda
+        existing_messages = client.iter_messages(channel_to_forward)
+        existing_message_ids = {message.id async for message in existing_messages}
+        existing_message_texts = {message.message.lower() if message.message else None async for message in existing_messages}
 
-        existing_messages = client.iter_messages(channel_to_forward);
-        existing_message_ids = [message.id async for message in existing_messages]
-        existing_message_texts = [message.message.lower() if message.message else None async for message in existing_messages]
         messages_to_forward = []
         message_ids = set()
         message_texts = set()
-        
+
         # Iterate through the channels
         for channel in channels:
             # Fetch the latest messages from the channel
             async for message in client.iter_messages(channel, limit=100):
-                #print(message.message)
-                if message.message and any(keyword in message.message.lower() for keyword in keywords) and not any(stopword in message.message.lower() for stopword in stopwords):
-                    # Forward the message to the specified channel
-                    if message.id not in message_ids and message.id not in existing_message_ids:
-                        msg = message.message.lower()
-                        if msg not in message_texts and msg not in existing_message_texts:
-                            message_texts.add(msg)
-                            message_ids.add(message.id)
-                            messages_to_forward.append(message)
+                if message.message:
+                    message_lower = message.message.lower()
+                    if any(keyword in message_lower for keyword in keywords) and not any(stopword in message_lower for stopword in stopwords):
+                        if message.id not in message_ids and message.id not in existing_message_ids:
+                            if message_lower not in message_texts and message_lower not in existing_message_texts:
+                                message_texts.add(message_lower)
+                                message_ids.add(message.id)
+                                messages_to_forward.append(message)
 
-        msg_count = len(messages_to_forward);
+        msg_count = len(messages_to_forward)
         for msg in messages_to_forward:
-            linkMsg = msg.message[:100] + f"...\nlink:\nhttps://t.me/c/{msg.peer_id.channel_id}/{msg.id}"
+            link_msg = f"{msg.message[:100]}...\nlink:\nhttps://t.me/c/{msg.peer_id.channel_id}/{msg.id}"
             await client.forward_messages(channel_to_forward, msg)
-            await client.send_message(channel_to_forward, linkMsg)
+            await client.send_message(channel_to_forward, link_msg)
+
         # Disconnect the client
         await client.disconnect()
 
